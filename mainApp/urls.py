70946d8a7efd323ea515e3b49f83a6c228b57a59
java2019 <java_2019@mail.ru>
2019-08-08 22:25:22 +0300

    *

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
 from django.conf.urls import include
+from . import views
 
 urlpatterns = [
-    url(r'^$/', include('mainApp.urls'),
-        url(r'^webexample/', include('webexample.urls')),
-        url(r'^admin/', admin.site.urls)),
+    url(r'^$/', views.index, name='index'),
 ]
